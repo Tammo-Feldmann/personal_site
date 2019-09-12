8d9838ff9dd507a45f07381030891f525d1671a5
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-09-12 10:29:57 -0700

    Moved content to separate sections for skills, projects, and education.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import React from "react"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Skills from "../components/skills"
+import Projects from "../components/projects"
 
 const IndexPage = () => (
   <Layout>
@@ -12,10 +14,11 @@ const IndexPage = () => (
     <div style={{ display: `flex` }}>
       <div> 
       <h1 style={{
-        color: `#2F3854`
+        color: `#353848`
 
       }}
       >About Me</h1>
+      
       <div style={{ 
         maxWidth: `300px`, 
         marginBottom: `1.45rem`, 
@@ -35,22 +38,12 @@ const IndexPage = () => (
     <hr style={{
       borderColor: '#d'
     }}></hr>
-
-    <p>- - - -Skills Construction hours per month -- working on my development stack</p>
+    <Skills></Skills>
+    <Projects></Projects>
     <h3>Education</h3>
       <p> - Stanford University, PhD in German Studies, 2018</p>
       <p> - Harvard University, Visiting Exchange Scholar, 2016</p>
       <p> - Carl von Ossietzky Universität, MA in Exercise Physiology, 2007</p>
-    <h3>Experience</h3>
-      <p>Full-Stack Intern, March 2019 - Present, Datamago</p>
-      <p>Learning from friends, Last years of Stanford time</p>
-    <h3>Expertise</h3>
-      <p>JavaScript, Python, React, Flask</p>
-      <p>HTML & CSS</p>
-      <p>Machine Learning</p>
-      <p>Time Series Forcasting</p>
-    <h3>Contact</h3>
-      <p>Email: tammo.feldmann@gmail.com</p>
   </Layout>
 )
 
