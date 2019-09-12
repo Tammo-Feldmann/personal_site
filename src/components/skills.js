93452105f9b9df396d0fbea1ebe92d973388efe1
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-09-12 10:48:27 -0700

    Added a profile sections files

---
+++ src/components/skills.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Skills = () => (
+  <skills>
+    <h3>Skill Development</h3>
+      <p>- - - -Skills Construction hours per month -- working on my development stack</p>
+      <p>JavaScript, Python, React, Flask</p>
+      <p>HTML & CSS</p>
+      <p>Machine Learning</p>
+      <p>Time Series Forcasting</p>
+      <hr style={{
+      borderColor: 'green'
+      }}></hr>
+  </skills>
+)
+
+export default Skills
