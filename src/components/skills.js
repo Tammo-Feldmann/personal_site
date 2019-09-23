3d7ecb5fec8e6c70234d5d58075934e9117004cb
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-09-23 13:21:57 -0700

    Finally figured out the alignment for the header. Also fixed a bug with a <p> tag around the clipboard component.

--- src/components/skills.js
+++ src/components/skills.js
@@ -2,9 +2,14 @@ import React from 'react'
 import Graph from './graph'
 
 const Skills = () => (
-  <div id="expertise-section" style={{paddingTop: `3rem`, color: `#353848` }}>
+  <div id="expertise-section" style={{paddingTop: `1rem`, color: `#353848` }}>
     <h1>Expertise</h1>
+    <p style={{maxWidth: 600}}>I have listed my various skills and their relationships in a sanky diagram. I really enjoyed seeing it and found it insightful. 
+      Thought I might share how I think, here. 
+    </p>
+    <div style={{textAlign:`center`, marginTop: `2rem`}}>
     <Graph></Graph>
+    </div>
     <div>- - - -Skills Construction hours per month -- working on my development stack</div>
     <div style={{marginTop: `4rem`, marginBottom: `4rem`}}>
       <p>JavaScript, Python, React, Flask</p>
