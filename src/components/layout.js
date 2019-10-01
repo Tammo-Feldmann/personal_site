61eb6f7f83b9937e12fd5433787523252f863dd5
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-01 11:11:28 -0700

    Added a triangle to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -34,9 +34,9 @@ const Layout = ({ children }) => {
         <div style={{ 
           display: `flex`, 
           justifyContent: `space-between`,
-          margin: `1rem`,
-          paddingTop: `1.5rem`,
-          width: `46rem`
+          margin: `.5rem`,
+          paddingTop: `1.75rem`,
+          width: `56rem`
           }}>
             <RsLink
               activeClass="active"
@@ -67,6 +67,15 @@ const Layout = ({ children }) => {
             </RsLink>
         </div>
         </div>
+        <div style={{
+          position: `absolute`,
+          width: `0`,
+          height: `0`,
+          borderRight: `6rem solid transparent`,
+          borderTop: `18rem solid #9CA7C8`,
+          zIndex: `2`
+        }}>
+          </div>
 
       <div style={{
         width: `0 auto`,
@@ -77,12 +86,15 @@ const Layout = ({ children }) => {
         }}>
       </div>
 
+
+
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
+          zIndex: `-1`
         }}
       >
         <main>{children}</main>
