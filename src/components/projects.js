4353ad0d2c7d18afb214888be063abda64a82e37
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-05 09:01:24 -0700

    Created first version of project cards in the project section

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Landing from '../images/dm-landing.png'
 
 const Projects = () => (
   <div id="projects-section" style={{marginTop:`1rem`}}>
     <h1>Projects</h1>
       <p>Here I will be including a bunch of screenshots from stuff that I am working on ...</p>
-      <p style={{margin:`12rem`}}>Just a place holder to test scrolling</p>
+      <div style={{marginTop:`1rem`}}>
+        <h3>Datamago</h3>
+        <p>Time series forecasting. Machine learning platform</p>
+        <img src={Landing} alt="Datamago Landing Page"></img>
+        {/* <a href=  */}
+      </div>
       <hr></hr>
   </div>
 )
