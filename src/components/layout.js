6fffe7bd9d3d0528f91b2444ed8ecbef706c4892
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-05 08:32:59 -0700

    Styled the skills section and added content

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,7 +33,7 @@ const Layout = ({ children }) => {
         position: `absolute`,
         width: `100%`,
         height: `16.5rem`,
-        backgroundColor: `#525C7B`,
+        backgroundColor: `#3F4967`,
         /* The points are: centered top, left bottom, right bottom */
         clipPath: `polygon(0 -1%, 100% -1%, 100% 21%, 98.6% 21%, 97% 16%, 95.4% 19.5%, 93.4% 12%, 91% 20%, 90% 14%, 89.4% 14.4%, 88.8% 14%, 87.8% 21%, 87% 16%, 86.2% 21%, 12% 22%, 0 100%)`,
         }}>
--- src/components/skills.js
+++ src/components/skills.js
@@ -7,16 +7,10 @@ const Skills = () => (
     <p style={{maxWidth: 600}}>I have listed my various skills and their relationships in a sanky diagram. I really enjoyed seeing it and found it insightful. 
       Thought I might share how I think, here. 
     </p>
-    <div style={{display: `flex`, justifyContent:`center`, marginTop: `2rem`, marginBottom: `2rem`}}>
-    </div>
-   
-    <img src={Sankey} alt="Sankey Diagram"></img>
-    <div>- - - -Skills Construction hours per month -- working on my development stack</div>
-    <div style={{marginTop: `4rem`, marginBottom: `4rem`}}>
-      <p>JavaScript, Python, React, Flask</p>
-      <p>HTML & CSS</p>
-      <p>Machine Learning</p>
-      <p>Time Series Forcasting</p>
+    <div style={{backgroundColor: `#F4F5F6`, borderRadius: `25px`, padding: `.3rem 2rem .3rem 2rem`, marginBottom: `4rem`}}>
+      <h3 style={{paddingTop: `2rem`}}>Skill Development</h3>
+      <p style={{maxWidth: `30rem`}}>I am working on building up the following skills and thought it might be nicer to see them in a sankey diagram than just a list</p>
+      <img src={Sankey} alt="Sankey Diagram"></img>
     </div>
     <hr></hr>
   </div>
