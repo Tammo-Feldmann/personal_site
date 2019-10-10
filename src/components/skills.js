839fdf725c8f230bbdf2aaee1a0991fe41336739
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-10 06:50:56 -0700

    Fixed color and styling of skills section

--- src/components/skills.js
+++ src/components/skills.js
@@ -21,7 +21,7 @@ const Skills = () => (
     <p style={{maxWidth: 600, color: `#FFFFFF`}}>I have listed my various skills and their relationships in a sanky diagram. I really enjoyed seeing it and found it insightful. 
       Thought I might share how I think, here. 
     </p>
-    <div style={{backgroundColor: `#FFFFFF`, borderRadius: `25px`, padding: `.3rem 2rem .3rem 2rem`, marginBottom: `4rem`}}>
+    <div style={{backgroundColor: `#4C5570`, borderRadius: `25px`, padding: `.3rem 2rem .3rem 2rem`, marginBottom: `4rem`, color: `#FAFAFA`}}>
       <h3 style={{paddingTop: `2rem`}}>Skill Development</h3>
       <p style={{maxWidth: `30rem`}}>I am working on building up the following skills and thought it might be nicer to see them in a sankey diagram than just a list</p>
       <img src={Sankey} alt="Sankey Diagram"></img>
