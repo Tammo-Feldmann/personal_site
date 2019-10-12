9a1fcc66daec0b12c05e7a7ba7d0476b46f9787d
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-11 18:12:41 -0700

    First implementation of new styling idea for projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,16 +3,17 @@ import Landing from '../images/dm-landing.png'
 
 const Projects = () => (
   <div id="projects-section" style={{
-    padding:`8rem 16% 4rem 16%`,
+    padding:`8rem 5% 4rem 5%`,
     backgroundColor: `#FAFAFA`,
+    width: `40%`
     }}>
     <h1>Projects</h1>
     <p>I work on most of my bigger projects collaboratively, either remotely or in person, the smaller projects in this
       section are my own and I will specify where I have written all the backend and frontend code.</p>
-    <div style={{borderLeft: `3rem solid #3F4967`, display: `flex`, justifyContent: `space-between`, padding: `2rem`, marginTop:`1rem`, backgroundColor: `#FFFFFF`, borderRadius: `15px`}}>
+    <div style={{borderLeft: `2rem solid #3F4967`, display: `flex`, justifyContent: `space-between`, margin: `2rem 2rem 2rem 120%`, backgroundColor: `#FFFFFF`, borderRadius: `15px`}}>
         <div>
           <h2>Datamago</h2>
-          <p style={{width:`20rem`}}>Datamago is a machine learning platform. The company focuses on time series forecasting. I have contributed to frontend features, 
+          <p style={{width:`100%`}}>Datamago is a machine learning platform. The company focuses on time series forecasting. I have contributed to frontend features, 
             data visualization, QA and CI pipelines, and others. 
           </p>
           <div style={{
@@ -22,7 +23,7 @@ const Projects = () => (
             backgroundColor: `#3F4967`,
             color: `white`,
             textAlign: `center`,
-            width: `12rem`
+            width: `70%`
             }}>
           <a href="https://www.linkedin.com/in/tammo-feldmann/"
           style={{color: `#FAFAFA`}}>More information
@@ -37,7 +38,7 @@ const Projects = () => (
             <ul>Cypress</ul>
           </div>
         </div>
-        <div style={{width: `20rem`, height: `15rem`}}>
+        <div style={{minWidth: `20rem`, height: `15rem`}}>
           <img src={Landing} alt="Datamago Landing Page" 
         ></img>
         </div>
