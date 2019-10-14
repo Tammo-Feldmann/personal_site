e48cb6a7c3465ccaeb25ed7c00bdd82eeaf1f57b
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-13 19:16:02 -0700

    Moved card content to project cards

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Landing from '../images/dm-landing.png'
 const { Component } = React;
 
 class ProjectCard extends Component {
@@ -7,12 +7,35 @@ class ProjectCard extends Component {
     const { title, excerpt } = this.props;
 
     return (
-      <div className="project-card" style={{margin: `1.5rem`, padding: `2rem 0rem 0rem 0rem`}}>
+      <div className="project-card" style={{border: `solid blue 1px`, margin: `1.5rem`, padding: `2rem 0rem 0rem 0rem`, boxShadow: "4px 6px 4px #3F4967", backgroundColor: `#FFFFFF`, borderRadius: `15px`,}}>
           <h2 className="card-header">
             {title}
           </h2>
           <div className="card-body" style={{maxWidth: `25rem`}}>
             {excerpt}
+          </div>
+          <div style={{
+            margin: `4rem 1rem 2rem 1rem`,
+            padding: `.8rem`,
+            borderRadius: `5px`,
+            backgroundColor: `#3F4967`,
+            color: `white`,
+            textAlign: `center`,
+            width: `70%`
+            }}>
+          <a href="https://www.linkedin.com/in/tammo-feldmann/"
+          style={{color: `#FAFAFA`}}>More information
+          </a>
+          </div>
+          <h4>Technologies</h4>
+          <div style={{display: `inline-flex`}}>
+            <ul>JavaScript</ul>
+            <ul>Firebase</ul>
+            <ul>Jest</ul>
+            <ul>Cypress</ul>
+          </div>
+          <div style={{minWidth: `20rem`, height: `15rem`}}>
+            <img src={Landing} alt="Datamago Landing Page"></img>
           </div>
       </div>
     );
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Landing from '../images/dm-landing.png'
 import ProjectsContainer from './projects-container'
 
 const Projects = () => (
@@ -14,41 +13,8 @@ const Projects = () => (
       section are my own and I will specify where I have written all the backend and frontend code.</p>
     </div>
     <div> 
-      <ProjectsContainer></ProjectsContainer>
-    <div style={{boxShadow: "4px 6px 4px #3F4967", display: `flex`, justifyContent: `space-between`, backgroundColor: `#FFFFFF`, borderRadius: `15px`, margin: `4rem`}}>
-        <div style={{backgroundColor: `#3F4967`, padding: `0rem 2rem 0rem 2rem`}}>
-          <h2>Datamago</h2>
-          <p style={{width:`80%`}}>Datamago is a machine learning platform. The company focuses on time series forecasting. I have contributed to frontend features, 
-            data visualization, QA and CI pipelines, and others. 
-          </p>
-          <div style={{
-            margin: `4rem 1rem 2rem 1rem`,
-            padding: `.8rem`,
-            borderRadius: `5px`,
-            backgroundColor: `#3F4967`,
-            color: `white`,
-            textAlign: `center`,
-            width: `70%`
-            }}>
-          <a href="https://www.linkedin.com/in/tammo-feldmann/"
-          style={{color: `#FAFAFA`}}>More information
-          </a>
-          </div>
-
-          <h4>Technologies</h4>
-          <div style={{display: `inline-flex`}}>
-            <ul>JavaScript</ul>
-            <ul>Firebase</ul>
-            <ul>Jest</ul>
-            <ul>Cypress</ul>
-          </div>
-        </div>
-        <div style={{minWidth: `20rem`, height: `15rem`}}>
-          <img src={Landing} alt="Datamago Landing Page" 
-        ></img>
-        </div>
-      </div>
-      </div>
+      <ProjectsContainer></ProjectsContainer>    
+    </div>
 </div>
 )
 
