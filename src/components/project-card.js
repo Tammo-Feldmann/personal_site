96b7ed0593ba151c5908bc2cd6dcfd62c8b34379
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-16 09:03:16 -0700

    Styling projects section into responsive grid

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -9,14 +9,12 @@ class ProjectCard extends Component {
     return (
       <div className="project-card" style={{
         border: `solid #E6E6E8 2px`,
-        padding: `2rem 1rem 0rem 1rem`, 
+        padding: `1rem 1rem 1rem 1rem`, 
         boxShadow: "2px 4px 2px #575964", 
         backgroundColor: `#D9D9DA`, 
         borderRadius: `15px`,
         margin: `1rem`,
-        width: `14rem`,
-        flex: `1`,
-        minWidth: `14rem`
+        width: `12rem`
         }}>
         <div>
           <img src={Landing} alt="Datamago Landing Page" ></img>
@@ -29,7 +27,7 @@ class ProjectCard extends Component {
             {excerpt}
           </div>
         </div>
-          <div style={{display:  `flex`, flexWrap: `wrap`, fontSize: `12px`, justifyContent: `space-between`, flexBasis: `20%`}}>
+          <div style={{display:  `flex`, flexWrap: `wrap`, fontSize: `12px`, justifyContent: `space-between`, flexBasis: `40%`}}>
             <div style={{flex: `1`}}>JavaScript</div>
             <div style={{flex: `1`}}>Firebase</div>
             <div style={{flex: `1`}}>Jest</div>
