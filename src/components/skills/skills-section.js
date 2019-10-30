352b4c33bcc37072561ce90f05e18e89e3736fc0
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-30 15:02:40 -0700

    Fixed some import errors from moving the skills files

--- src/components/skills/skills-section.js
+++ src/components/skills/skills-section.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SkillCard from './skill-card';
-import Polygon from './polygon'
-import SkillStyles from "./skills.module.css"
+import Polygon from '../polygon'
+import SkillStyles from "./skills-section.module.css"
 
 export default ({ children }) => (
   <div className={SkillStyles.container}>
@@ -9,9 +9,9 @@ export default ({ children }) => (
     <div id="expertise-section" className={SkillStyles.skillsSection}>
       <div className={SkillStyles.summary}>
         <h1>Expertise</h1>
-        <p className={SkillStyles.text}>I am just comfortable with backend problems as I like to work on frontend features. 
-            Most of my work has been in Javascript/React and Python on the backend. 
-            More recently I have been really fascinated with Rust. 
+        <p className={SkillStyles.text}>I am just comfortable with backend problems as I like to work on frontend features.
+            Most of my work has been in Javascript/React and Python on the backend.
+            More recently I have been really fascinated with Rust.
         </p>
       </div>
       <div className={SkillStyles.cardsContainer}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 //import SEO from "../components/seo"
-import Skills from "../components/skills"
+import Skills from "../components/skills/skills-section"
 import Projects from "../components/projects/projects-section"
 import About from "../components/about"
 
