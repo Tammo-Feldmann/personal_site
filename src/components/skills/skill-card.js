416655494d46a2bf1f9730047918cdefc18ea233
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-10-31 15:18:38 -0700

    Refactored skill-card to be a class based component

--- src/components/skills/skill-card.js
+++ src/components/skills/skill-card.js
@@ -1,17 +1,24 @@
-import React from "react"
+import React, { Component } from "react"
 import SkillCardStyles from "./skill-card.module.css"
 
-export default ({ children }) => (
-  <div className={SkillCardStyles.container}>
-    <div className={SkillCardStyles.icon}>
-    </div>
-    <div className={SkillCardStyles.spacer}> </div>
-    <div className={SkillCardStyles.text}>
-      <h4>This is my Skill Card</h4>
+class SkillCard extends Component {
+  render() {
+    return (
+      <div className={SkillCardStyles.container}>
+        <div className={SkillCardStyles.icon}>
+        </div>
+        <div className={SkillCardStyles.spacer}> </div>
+        <div className={SkillCardStyles.text}>
+          <h4>This is my Skill Card</h4>
 
-      <p>Some description of my skill. I have been developing with Javascript and Python React frameworks. I like to learn about
-       browsers and all the intricacies of cool web-technologies. I am currently learning Rust and mobile development with React native.
+          <p>Some description of my skill. I have been developing with Javascript and Python React frameworks. I like to learn about
+           browsers and all the intricacies of cool web-technologies. I am currently learning Rust and mobile development with React native.
         </p>
-    </div>
-  </div>
-);
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default SkillCard
