7c8ad6e59ecc2d95d0e6b3d17f99040a58a820bf
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-11-08 13:38:02 -0800

    Fixed image import in experience section

--- src/components/experience/experience-section.js
+++ src/components/experience/experience-section.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Row, Button, Card, CardBody, CardImg, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 //import experienceStyles from "./experience-section.module.css"
+import img from "../../images/ABCyaScreen.png"
 
 const Experience = () => (
   <div className={CardBody}>
@@ -8,7 +9,7 @@ const Experience = () => (
     <Row>
       <Col sm="6">
         <Card>
-          <CardImg top width="100%" src="../images/ABCyaScreen.png" alt="Card image cap" />
+          <CardImg top width="100%" src={img} alt="Card image cap" />
           <CardBody>
             <CardTitle>Card title</CardTitle>
             <CardSubtitle>Card subtitle</CardSubtitle>
@@ -19,7 +20,18 @@ const Experience = () => (
       </Col>
       <Col sm="6">
         <Card>
-          <CardImg top width="100%" src="../images/ABCyaScreen.png" alt="Card image cap" />
+          <CardImg top width="100%" src={img} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </Col>
+      <Col sm="6">
+        <Card>
+          <CardImg top width="100%" src={img} alt="Card image cap" />
           <CardBody>
             <CardTitle>Card title</CardTitle>
             <CardSubtitle>Card subtitle</CardSubtitle>
