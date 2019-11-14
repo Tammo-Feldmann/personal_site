ce213562c84f35cb47f8417799038bd409c5c29e
Tammo Feldmann <tammo.feldmann@gmail.com>
2019-11-14 13:30:07 -0800

    Cleaned up Skill Cards and some other styling

--- src/components/skills/skill-card.js
+++ src/components/skills/skill-card.js
@@ -3,16 +3,33 @@ import { Card } from "react-bootstrap"
 
 class SkillCard extends Component {
   render() {
-    const { title, excerpt, header } = this.props //Remember to include Icon here if you want to use it again
+    const { title, excerpt } = this.props //Remember to include Icon here if you want to use it again
     return (
       <div>
-        <Card bg="light" text="dark" style={{ width: "32rem" }}>
-          <Card.Header>{header}</Card.Header>
-          <Card.Body>
-            {/* <Card.Img variant="bottom" src={img} /> */}
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{excerpt}</Card.Text>
-          </Card.Body>
+        <Card
+          text="light"
+          border="None"
+          style={{
+            backgroundColor: "#3f4967",
+            width: "32rem",
+            borderColor: "#3f4967",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "2rem",
+            }}
+          >
+            <div>Icon Placehoder</div>
+            <Card.Body>
+              {/* <Card.Img variant="bottom" src={img} /> */}
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>{excerpt}</Card.Text>
+            </Card.Body>
+          </div>
         </Card>
         <br />
       </div>
