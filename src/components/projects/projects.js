61674e244a9a58f496cbd04109789e3885df613f
Tammo Feldmann <tammo.feldmann@gmail.com>
2021-01-30 07:25:54 -0500

    Add initial responsive styles to projects

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -4,17 +4,15 @@ import containerStyles from "./projects.module.css"
 
 const Projects = () => (
   <div className={containerStyles.projects}>
-    <div id="projects-section" className={containerStyles.inner}>
-      <div className={containerStyles.header_wrapper}>
-        <div className={containerStyles.header}>
-          <h1>Projects</h1>
-          <p>I work on most of my bigger projects collaboratively, either remotely or in person, the smaller projects in this
-          section are my own and I will specify where I have written all the backend and frontend code.
+    <div className={containerStyles.header_wrapper}>
+      <div className={containerStyles.header}>
+        <h1>Projects</h1>
+        <p>I work on most of my bigger projects collaboratively, either remotely or in person, the smaller projects in this
+        section are my own and I will specify where I have written all the backend and frontend code.
         </p>
-        </div>
       </div>
-      <ProjectsContainer></ProjectsContainer>
     </div>
+    <ProjectsContainer></ProjectsContainer>
   </div>
 )
 
