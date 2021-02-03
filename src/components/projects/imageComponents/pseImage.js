2bd64019aec399ca53d3df7c03ab9982c109d4b9
Tammo Feldmann <tammo.feldmann@gmail.com>
2021-02-03 06:53:33 -0500

    Fix graphql import warning

--- src/components/projects/imageComponents/pseImage.js
+++ src/components/projects/imageComponents/pseImage.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { StaticQuery } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
 import BackgroundImage from 'gatsby-image'
 
 const ProjectImage = ({ className }) => (
-    <StaticQuery
-        query={graphql`
+  <StaticQuery
+    query={graphql`
         query {
           desktop: file(relativePath: { eq: "pse-logo.png" }) {
             childImageSharp {
@@ -15,20 +15,20 @@ const ProjectImage = ({ className }) => (
           }
         }
         `}
-        render={data => {
-            const imageData = data.desktop.childImageSharp.fluid
-            return (
-                <BackgroundImage
-                    Tag="section"
-                    className={className}
-                    fluid={imageData}
-                    backgroundColor={`#ffffff`}
-                    style={{ height: "7rem" }}
-                >
-                </BackgroundImage>
-            )
-        }}
-    />
+    render={data => {
+      const imageData = data.desktop.childImageSharp.fluid
+      return (
+        <BackgroundImage
+          Tag="section"
+          className={className}
+          fluid={imageData}
+          backgroundColor={`#ffffff`}
+          style={{ height: "7rem" }}
+        >
+        </BackgroundImage>
+      )
+    }}
+  />
 )
 
 export default ProjectImage
