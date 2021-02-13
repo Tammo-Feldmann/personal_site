8131bfedb1827ad935b6fe7f6862dde2dd314ca8
Tammo Feldmann <tammo.feldmann@gmail.com>
2021-02-13 07:28:58 -0500

    Hide scroll menu for tablet and mobile

--- src/components/poly1.js
+++ src/components/poly1.js
@@ -15,55 +15,56 @@ const Poly1 = () => {
         clipPath: `polygon(0 -1%, 100% -1%, 100% 21%, 98.6% 21%, 97% 16%, 95.4% 19.5%, 93.4% 12%, 91% 20%, 90% 14%, 89.4% 14.4%, 88.8% 14%, 87.8% 21%, 87% 16%, 86.2% 21%, 15% 22%, 0 ${windowWidth <= 414 ? "22%" : windowWidth <= 768 ? "65%" : "100%"})`,
       }}
     >
-      <div
-        style={{
-          display: `flex`,
-          justifyContent: `center`,
-          marginLeft: `4rem`,
-        }}
-      >
+      { windowWidth > 1024 &&
         <div
           style={{
             display: `flex`,
-            /* The values represent: top, right, bottom, left */
-            padding: `1rem 8rem 0rem 0rem`,
-            justifyContent: `space-between`,
-            width: `50rem`,
-            minWidth: `28rem`,
+            justifyContent: `center`,
+            marginLeft: `4rem`,
           }}
         >
-          <RsLink
-            activeClass="active"
-            to="about-section"
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
+          <div
+            style={{
+              display: `flex`,
+              /* The values represent: top, right, bottom, left */
+              padding: `1rem 8rem 0rem 0rem`,
+              justifyContent: `space-between`,
+              width: `50rem`,
+              minWidth: `28rem`,
+            }}
           >
-            <HoverText>about</HoverText>
-          </RsLink>
-          <RsLink
-            activeClass="active"
-            to="skills-section"
-            spy={true}
-            smooth={true}
-            offset={120}
-            duration={700}
-          >
-            <HoverText>skills</HoverText>
-          </RsLink>
-          <RsLink
-            activeClass="active"
-            to="experience-section"
-            spy={true}
-            smooth={true}
-            offset={-20}
-            duration={1100}
-          >
-            <HoverText>projects</HoverText>
-          </RsLink>
-        </div>
-      </div>
+            <RsLink
+              activeClass="active"
+              to="about-section"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <HoverText>about</HoverText>
+            </RsLink>
+            <RsLink
+              activeClass="active"
+              to="skills-section"
+              spy={true}
+              smooth={true}
+              offset={120}
+              duration={700}
+            >
+              <HoverText>skills</HoverText>
+            </RsLink>
+            <RsLink
+              activeClass="active"
+              to="experience-section"
+              spy={true}
+              smooth={true}
+              offset={-20}
+              duration={1100}
+            >
+              <HoverText>projects</HoverText>
+            </RsLink>
+          </div>
+        </div>}
     </div>
   )
 }
